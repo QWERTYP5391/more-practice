e36bc3ced1734e8b28fd9b1b60f60cc6b3871bf3
Ebrima Tunkara <etunkara51@gmail.com>
2019-03-03 12:54:27 -0500

    Going to put sort all in one class.

---
+++ src/main/java/MySort.java
@@ -0,0 +1,30 @@
+public class MySort {
+
+    public static int[] bubbleSort(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < (array.length - i) - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    swap(array, j, j + 1);
+                }
+            }
+        }
+
+        return array;
+
+    }
+
+
+    public static void swap(int[] array, int i, int j) {
+
+        if (i == j) {
+            return;
+        }
+
+        int temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+
+    }
+}
+
---
+++ src/test/java/MySortTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.*;
+
+public class MySortTest {
+
+    @Test
+    public void sort() {
+        int [] input = {3, 6, 1, 9};
+        int [] expected = {1, 3, 6, 9};
+
+
+        assertThat(MySort.bubbleSort(input), equalTo(expected));
+    }
+}
