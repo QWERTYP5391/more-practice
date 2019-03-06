1ecd3c2819a1a238c8d2909b158edc22f62966a1
Ebrima Tunkara <etunkara51@gmail.com>
2019-03-05 22:53:56 -0500

    Add some test cases for Array Chunk

--- src/main/java/ArrayChunk.java
+++ src/main/java/ArrayChunk.java
@@ -1,8 +1,29 @@
+import java.util.Arrays;
 
 public class ArrayChunk {
 
     public static int[][] chunk(int[] array, int size) {
 
-        return new int[0][];
+        if (array == null || array.length < 1) {
+            return null;
+        }
+
+        int length = array.length;
+
+        int rows = length / size;
+        rows = (length % size) == 0 ? rows : rows + 1;
+        int[][] ints = new int[rows][];
+        int index = 0;
+        int slice = 0;
+
+        while (index < rows) {
+            int to = slice + size < length ? slice + size : length;
+            ints[index] = Arrays.copyOfRange(array, slice, to);
+            slice += size;
+            index++;
+        }
+
+
+        return ints;
     }
 }
--- src/test/java/ArrayChunkTest.java
+++ src/test/java/ArrayChunkTest.java
@@ -1,14 +1,12 @@
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
-@Ignore
 public class ArrayChunkTest {
 
     @Test
-    public void chunk() {
+    public void chunk1234Size2() {
         int[] input = {1, 2, 3, 4};
         int size = 2;
         int[][] expected = {{1, 2}, {3, 4}};
@@ -16,4 +14,46 @@ public class ArrayChunkTest {
 
         assertThat(ArrayChunk.chunk(input, size), equalTo(expected));
     }
+
+    @Test
+    public void chunk12345Size2() {
+        int[] input = {1, 2, 3, 4, 5};
+        int size = 2;
+        int[][] expected = {{1, 2}, {3, 4}, {5}};
+
+
+        assertThat(ArrayChunk.chunk(input, size), equalTo(expected));
+    }
+
+    @Test
+    public void chunk123456Size2() {
+        int[] input = {1, 2, 3, 4, 5, 6};
+        int size = 2;
+        int[][] expected = {{1, 2}, {3, 4}, {5, 6}};
+
+
+        assertThat(ArrayChunk.chunk(input, size), equalTo(expected));
+    }
+
+    @Test
+    public void chunk123456Size3() {
+        int[] input = {1, 2, 3, 4, 5, 6};
+        int size = 3;
+        int[][] expected = {{1, 2, 3}, {4, 5, 6}};
+
+
+        assertThat(ArrayChunk.chunk(input, size), equalTo(expected));
+    }
+
+    @Test
+    public void chunk12345678Size3() {
+        int[] input = {1, 2, 3, 4, 5, 6, 7, 8};
+        int size = 3;
+        int[][] expected = {{1, 2, 3}, {4, 5, 6}, {7, 8}};
+
+
+        assertThat(ArrayChunk.chunk(input, size), equalTo(expected));
+    }
+
+
 }
