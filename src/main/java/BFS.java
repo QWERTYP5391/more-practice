94db78908d702fcecf88b8565d4e02a49cc34a54
Ebrima Tunkara <etunkara51@gmail.com>
2019-08-14 23:27:47 -0400

    add bfs.

---
+++ src/main/java/BFS.java
@@ -0,0 +1,80 @@
+// IMPORT LIBRARY PACKAGES NEEDED BY YOUR PROGRAM
+// SOME CLASSES WITHIN A PACKAGE MAY BE RESTRICTED
+// DEFINE ANY CLASS AND METHOD NEEDED
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+// CLASS BEGINS, THIS CLASS IS REQUIRED
+public class BFS {
+
+    private static final int OBSTACLE = 9;
+
+    // METHOD SIGNATURE BEGINS, THIS METHOD IS REQUIRED
+    int removeObstacle(int numRows, int numColumns, List<List<Integer>> lot) {
+        // WRITE YOUR CODE HERE
+        boolean[][] visited = new boolean[numRows][numColumns];
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(new Node(0, 0, lot.get(0).get(0), 0));
+        return distanceHelper(lot, queue, visited);
+    }
+
+    // METHOD SIGNATURE ENDS
+    private int distanceHelper(List<List<Integer>> lot, Queue<Node> queue, boolean[][] visited) {
+        while (!queue.isEmpty()) {
+            Node node = queue.remove();
+            if (lot.get(node.row).get(node.column).equals(OBSTACLE)) {
+                return node.distance;
+            }
+            List<Node> directions = getDirections(node, lot, visited);
+            queue.addAll(directions);
+        }
+
+        return 0;
+
+    }
+
+    private List<Node> getDirections(Node node, List<List<Integer>> lot, boolean[][] visited) {
+        List<Node> directions = new ArrayList<>();
+
+        if (node.row + 1 < lot.size() && node.column < lot.get(0).size() && !lot.get(node.row + 1).get(node.column).equals(0) && !visited[node.row + 1][node.column]) {
+            visited[node.row + 1][node.column] = true;
+            directions.add(new Node(node.row + 1, node.column, lot.get(node.row + 1).get(node.column), node.distance + 1));
+        }
+
+        if (node.row - 1 >= 0 && node.row - 1 < lot.size() && node.column < lot.get(0).size() && !lot.get(node.row - 1).get(node.column).equals(0) && !visited[node.row - 1][node.column]) {
+            visited[node.row - 1][node.column] = true;
+            directions.add(new Node(node.row - 1, node.column, lot.get(node.row - 1).get(node.column), node.distance + 1));
+        }
+
+        if ((node.row < lot.size() && node.column + 1 < lot.get(0).size()) && (node.value != 0 && !visited[node.row][node.column + 1])) {
+            visited[node.row][node.column + 1] = true;
+            directions.add(new Node(node.row, node.column + 1, lot.get(node.row).get(node.column + 1), node.distance + 1));
+        }
+
+        if (node.row < lot.size() - 1 && node.column - 1 >= 0 && node.column - 1 < lot.get(0).size() && !lot.get(node.row).get(node.column - 1).equals(0) && !visited[node.row][node.column - 1]) {
+            visited[node.row][node.column - 1] = true;
+            directions.add(new Node(node.row, node.column - 1, lot.get(node.row).get(node.column - 1), node.distance + 1));
+        }
+
+        return directions;
+
+    }
+
+    static class Node {
+        int row;
+        int column;
+        int value;
+        int distance;
+
+        Node(int row, int column, int value, int distance) {
+            this.row = row;
+            this.column = column;
+            this.value = value;
+            this.distance = distance;
+        }
+    }
+}
+
---
+++ src/test/java/BFSTest.java
@@ -0,0 +1,52 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.*;
+
+public class BFSTest {
+
+    @Test
+    public void removeObstacle() {
+        List<List<Integer>> input = new ArrayList<>();
+        List<Integer> one = new ArrayList<>();
+        one.add(1);
+        one.add(1);
+        one.add(1);
+        one.add(1);
+        input.add(one);
+
+        List<Integer> two = new ArrayList<>();
+        two.add(0);
+        two.add(1);
+        two.add(1);
+        two.add(1);
+        input.add(two);
+
+        List<Integer> three = new ArrayList<>();
+        three.add(0);
+        three.add(1);
+        three.add(0);
+        three.add(1);
+        input.add(three);
+
+        List<Integer> four = new ArrayList<>();
+        four.add(1);
+        four.add(1);
+        four.add(9);
+        four.add(1);
+        input.add(four);
+
+        List<Integer> five = new ArrayList<>();
+        five.add(0);
+        five.add(0);
+        five.add(1);
+        five.add(1);
+        input.add(five);
+
+
+        assertThat(new BFS().removeObstacle(input.size(), input.get(0).size(), input), equalTo(5));
+    }
+}
